ee114894aa6dd1e67cfdf241697ea7681394b924
lupeMorales <morales.lupegmail.com>
2022-08-03 11:28:38 +0200

    add timer

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 "user strict";
 const penguins = document.querySelectorAll(".js-penguin");
 const score = document.querySelector(".js-score");
+const time = document.querySelector(".js-time");
+const warning = document.querySelector(".js-warning");
 
 let count = 0;
+let timer = 6;
 
 function scoreCounter() {
   count++;
+  score.innerHTML = count;
+}
+function stopCounter() {
+  count = count;
   score.innerHTML = count;
 }
 function addPenguinListener() {
@@ -14,7 +21,21 @@ function addPenguinListener() {
   }
 }
 function handleClickPeguin(ev) {
-  scoreCounter();
+  timer !== 0 ? scoreCounter() : stopCounter();
+}
+function countDown() {
+  timer--;
+  time.innerHTML = timer;
+  if (timer === 0) {
+    timeOver();
+  }
+}
+id = setInterval(countDown, 1000);
+
+function timeOver() {
+  clearInterval(id);
+  warning.innerHTML = "game over!";
 }
 
 addPenguinListener();
+countDown();
