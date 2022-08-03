7dec565acc5355f3557b38244bfd0b4bf081777e
lupeMorales <morales.lupegmail.com>
2022-08-04 01:06:46 +0200

    add button playAgain

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 "user strict";
 const penguins = document.querySelectorAll(".js-penguin");
 const score = document.querySelector(".js-score");
+const totalScore = document.querySelector(".js-actualScore");
+const btnPlayAgain = document.querySelector(".js-playAgain");
 const time = document.querySelector(".js-time");
 const warning = document.querySelector(".js-warning");
 const windowModal = document.querySelector(".js-modal");
@@ -10,6 +12,11 @@ const initialModal = document.querySelector(".js-modalCount");
 let count = 0;
 let timer = 14;
 let modalCount = 4;
+function reset() {
+  count = 0;
+  timer = 14;
+  modalCount = 4;
+}
 
 function scoreCounter() {
   count++;
@@ -26,6 +33,7 @@ function stopPenguins() {
 }
 function showScore() {
   windowScore.classList.remove("hidden");
+  totalScore.innerHTML = count;
 }
 
 function addPenguinListener() {
@@ -35,6 +43,14 @@ function addPenguinListener() {
 }
 function handleClickPeguin(ev) {
   timer !== 0 ? scoreCounter() : stopCounter();
+}
+function handleClickPlayAgain(ev) {
+  ev.preventDefault();
+  console.log("no misiela");
+  windowScore.classList.add("hidden");
+  windowModal.classList.remove("hidden");
+  reset();
+  startGame();
 }
 function countDown() {
   timer--;
@@ -47,20 +63,23 @@ function countDown() {
 id = setInterval(countDown, 1000);
 
 function startGame() {
+  console.log("empieza el juego");
   modalCount--;
   initialModal.innerHTML = modalCount;
   if (modalCount === 0) {
     initialModal.innerHTML = "Go";
+    clearInterval(idInitialCount);
     windowModal.classList.add("hidden");
     countDown();
   }
 }
-setInterval(startGame, 1000);
+idInitialCount = setInterval(startGame, 1000);
 
 function timeOver() {
   clearInterval(id);
   warning.innerHTML = "game over!";
   stopPenguins();
 }
+btnPlayAgain.addEventListener("click", handleClickPlayAgain);
 startGame();
 addPenguinListener();
